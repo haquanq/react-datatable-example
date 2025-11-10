51b4ed16c6497b3afb39e3c3cba4ba0b4ad0eff2
Ha Quanq <quangha0120010@gmail.com>
2025-11-11 03:52:11 +0700

    Add danger Button variant

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/utils/cn";
 
 interface ButtonProps extends React.ComponentProps<"button"> {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "danger";
 }
 
 export const Button = ({ className, variant = "primary", children, ...restProps }: ButtonProps) => {
@@ -12,6 +12,7 @@ export const Button = ({ className, variant = "primary", children, ...restProps 
         {
           "border-gray-900 bg-gray-900 text-gray-50 hover:bg-white hover:text-gray-900": variant === "primary",
           "border-gray-200 bg-gray-50 text-gray-900 hover:border-gray-900": variant === "secondary",
+          "border-red-700 bg-red-700 text-white hover:bg-white hover:text-red-700": variant === "danger",
         },
         className,
       )}
