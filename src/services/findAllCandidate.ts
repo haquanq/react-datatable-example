909390956c169425b7cc33f24d5522c4711426bb
Ha Quanq <quangha0120010@gmail.com>
2025-11-10 11:46:05 +0700

    Add candidate service

---
+++ src/services/findAllCandidate.ts
@@ -0,0 +1,20 @@
+import type { Candidate } from "@/@types/Candidate";
+import { GENDERS } from "@/constants/Genders";
+import { faker } from "@faker-js/faker";
+
+export const findAllCandidate = (): Candidate[] => {
+    const gender = faker.helpers.arrayElement(Object.values(GENDERS));
+
+    return Array.from(
+        { length: 500 },
+        (_, index) =>
+            ({
+                id: index,
+                name: faker.person.fullName({ sex: gender }),
+                dateOfBirth: faker.date.birthdate({ min: 18, max: 60, mode: "age" }),
+                gender,
+                email: faker.internet.email(),
+                address: faker.location.streetAddress(),
+            }) satisfies Candidate,
+    );
+};
